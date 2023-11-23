bebfff3bcd3c10e755405c4fd641018991234c30
Spongey21 <driton.latifi101@gmail.com>
2023-11-23 01:26:15 +0100

    ,

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import "./style.scss";
 import "./javascript/stories";
 import "./javascript/archive";
+
+if (window.location.href.includes('settings')) {
+    const CHECKBOX = document.querySelector('.options__checkbox')
+
+    CHECKBOX.addEventListener('change', () => {
+        CHECKBOX.checked ? console.log('checked') : console.log('unchecked');
+    })
+}
