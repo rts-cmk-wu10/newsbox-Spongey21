d43450051ed2dc6d734b8086ee94d59b32039a3c
Spongey21 <driton.latifi101@gmail.com>
2023-11-27 10:22:01 +0100

    updated localstorage handler

--- src/javascript/handlers/localstorage.js
+++ src/javascript/handlers/localstorage.js
@@ -1,13 +1,3 @@
-export function storeData(title, text, section) {
-	if (localStorage.getItem(title)) return
-
-	localStorage.setItem(title, JSON.stringify({
-		title: title,
-		text: text,
-		section: section
-	}))
-}
-
 export function getData(keyword) {
 	return localStorage.getItem(keyword)
 }
